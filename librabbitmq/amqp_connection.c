b852f846c46a3bb9451f99844edfbea41f3289f1
Alan Antonuk <alan.antonuk@gmail.com>
2014-02-17 19:28:56 -0800

    Add missing amqp_get_server_properties() function.
    Fixes #170

--- librabbitmq/amqp_connection.c
+++ librabbitmq/amqp_connection.c
@@ -519,3 +519,8 @@ int amqp_send_frame(amqp_connection_state_t state,
 
   return res;
 }
+amqp_table_t *
+amqp_get_server_properties(amqp_connection_state_t state)
+{
+  return &state->server_properties;
+}
