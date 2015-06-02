784a0e9078bc1c4145b0cb0a86045fe0f72fb691
Alan Antonuk <alan.antonuk@gmail.com>
2015-06-01 23:39:58 -0700

    ycm: add -std=gnu90 -DHAVE_POLL flags to ycm config.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -43,7 +43,7 @@ flags = [
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c89',
+'-std=gnu90',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
@@ -51,6 +51,7 @@ flags = [
 '-x',
 'c',
 '-I', './librabbitmq',
+'-D', 'HAVE_POLL',
 ]
 
 
