5a75fbed8a4a86b612c9ecb83f78ca59e43e2838
Alan Antonuk <alan.antonuk@gmail.com>
2021-04-06 03:28:15 +0000

    Move public header files to include directory
    Signed-off-by: GitHub <noreply@github.com>

--- cmake/VersionFunctions.cmake
+++ cmake/VersionFunctions.cmake
@@ -1,7 +1,7 @@
 function(get_library_version VERSION_ARG)
-    file(STRINGS librabbitmq/amqp.h _API_VERSION_MAJOR REGEX "^#define AMQP_VERSION_MAJOR [0-9]+$")
-    file(STRINGS librabbitmq/amqp.h _API_VERSION_MINOR REGEX "^#define AMQP_VERSION_MINOR [0-9]+$")
-    file(STRINGS librabbitmq/amqp.h _API_VERSION_PATCH REGEX "^#define AMQP_VERSION_PATCH [0-9]+$")
+    file(STRINGS include/amqp.h _API_VERSION_MAJOR REGEX "^#define AMQP_VERSION_MAJOR [0-9]+$")
+    file(STRINGS include/amqp.h _API_VERSION_MINOR REGEX "^#define AMQP_VERSION_MINOR [0-9]+$")
+    file(STRINGS include/amqp.h _API_VERSION_PATCH REGEX "^#define AMQP_VERSION_PATCH [0-9]+$")
 
     string(REGEX MATCH "[0-9]+" _API_VERSION_MAJOR ${_API_VERSION_MAJOR})
     string(REGEX MATCH "[0-9]+" _API_VERSION_MINOR ${_API_VERSION_MINOR})
