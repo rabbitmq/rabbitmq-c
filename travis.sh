7605238ef80edb586d80d30ad2f5c1fcdd6de6d3
Alan Antonuk <alan.antonuk@gmail.com>
2021-04-19 07:02:20 +0000

    cmake: change BUILD_EXAMPLES to default to OFF
    This makes the project easier to use as an add_subdirectory in a larger
    project without needing to explicitly disable this feature.
    
    As a minor side-effect: BUILD_EXAMPLES now requires
    BUILD_SHARED_LIBS=ON. While there isn't a technical limitation, it makes
    the CMake code easier to reason about, and is believed this will not
    impact a lot of users. If this ends up being a problem, it can be
    reconsidered.

--- travis.sh
+++ travis.sh
@@ -2,14 +2,14 @@
 
 build_cmake() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../_install -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Wno-implicit-fallthrough -Werror" 
+  cmake .. -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=$PWD/../_install -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Wno-implicit-fallthrough -Werror" 
   cmake --build . --target install
   ctest -V .
 }
 
 build_macos() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../_install -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Werror" \
+  cmake .. -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=$PWD/../_install -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Werror" \
     -DOPENSSL_ROOT_DIR="/usr/local/opt/openssl@1.1"
   cmake --build . --target install
   ctest -V .
@@ -24,7 +24,7 @@ build_format() {
 
 build_coverage() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_BUILD_TYPE=Coverage -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
+  cmake .. -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Coverage -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Werror -fprofile-arcs -ftest-coverage"
   cmake --build . --target install
   ctest -V .
@@ -35,7 +35,7 @@ build_coverage() {
 
 build_asan() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
+  cmake .. -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Werror -fsanitize=address,undefined -O1"
   cmake --build . --target install
   ctest -V .
@@ -43,7 +43,7 @@ build_asan() {
 
 build_tsan() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
+  cmake .. -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Werror -fsanitize=thread,undefined -O1"
   cmake --build . --target install
   ctest -V .
@@ -52,7 +52,7 @@ build_tsan() {
 build_scan-build() {
   sudo apt-get install -y clang-tools
   mkdir $PWD/_build && cd $PWD/_build
-  scan-build cmake .. -DCMAKE_BUILD_TYPE=Debug \
+  scan-build cmake .. -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Wall -Wextra -Wstrict-prototypes -Wno-unused-function -Werror"
