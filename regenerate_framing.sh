f57cfd615971d0109e8a10e318214efdca3ecf7b
Alan Antonuk <alan.antonuk@gmail.com>
2021-04-21 05:18:40 +0000

    lib: add tool to framing.h/amqp_framing.c
    Signed-off-by: GitHub <noreply@github.com>

---
+++ regenerate_framing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+RMQ_VERSION=3.8.14
+DATA=${PWD}/codegen/rabbitmq-server-${RMQ_VERSION}/deps/rabbitmq_codegen/amqp-rabbitmq-0.9.1.json
+export PYTHONPATH=${PWD}/codegen/rabbitmq-server-${RMQ_VERSION}/deps/rabbitmq_codegen
+
+rm -rf codegen
+mkdir codegen
+
+wget -c https://github.com/rabbitmq/rabbitmq-server/releases/download/v${RMQ_VERSION}/rabbitmq-server-${RMQ_VERSION}.tar.xz -O - | tar -xJ -C codegen
+
+python2 librabbitmq/codegen.py header ${DATA} include/rabbitmq-c/framing.h
+python2 librabbitmq/codegen.py body ${DATA} librabbitmq/amqp_framing.c
+
+clang-format -i include/rabbitmq-c/framing.h librabbitmq/amqp_framing.c
